2782631a5998d89031fd470925386602eac057cd
Danyl Gryn <gryndanya@gmail.com>
2023-07-12 15:48:11 +0300

    1

---
+++ src/main/java/com/example/testwrapper/TestWrapperApplication.java
@@ -0,0 +1,13 @@
+package com.example.testwrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWrapperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestWrapperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testwrapper/TestWrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testwrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestWrapperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
